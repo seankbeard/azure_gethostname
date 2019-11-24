adc3cbc8ee605533d3f926f6d4dd9297acfe81d9
Sean Beard <sean.beard@dimensiondata.com>
2019-11-25 08:45:24 +1300

    initial commit

---
+++ lookup_hostname.ps1
@@ -0,0 +1,21 @@
+﻿$list = @()
+$basename = "demo-nationalparks-app"
+$zeropad = 2
+$azservers = az vm list --query '[].{Name:name}' -o tsv
+
+foreach ($server in $azservers)
+    {
+    if ($server -like "$basename*")
+        {
+        $list += $server
+        }
+    }
+
+$list = $list -replace "[^0-9]"
+$list = [array]::IndexOf($list, ($list | sort-object | Select -last 1))
+$nextnumber = $list +1
+$nextnumber = ([string]$nextnumber).PadLeft($zeropad,'0')
+
+$newcomputername = $basename + $nextnumber
+write-host Name: $newcomputername
+
